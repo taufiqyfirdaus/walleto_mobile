01a1273eb0780a14e3ac4c54b922ff315d04c21c
abiyasa05 <abiyasap24@gmail.com>
2023-12-23 10:11:48 +0700

    update bottom navigation controller

--- lib/app/modules/bottomnavigation/controllers/bottomnavigation_controller.dart
+++ lib/app/modules/bottomnavigation/controllers/bottomnavigation_controller.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
 import 'package:walleto_mobile/app/modules/balance/views/balance_view.dart';
 import 'package:walleto_mobile/app/modules/home/views/home_view.dart';
+import 'package:walleto_mobile/app/modules/profile/views/profile_view.dart';
+import 'package:walleto_mobile/app/modules/notification/views/notification_view.dart';
 
 class BottomnavigationController extends GetxController {
   RxInt CurrentIndex = 0.obs;
@@ -8,6 +10,8 @@ class BottomnavigationController extends GetxController {
   final screens = [
     HomeView(),
     BalanceView(),
+    NotificationView(),
+    ProfileView(),
   ];
 
   SetIndex(index) {
